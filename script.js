51870ba7676d2e7ba1a27a2568a3637a8bdc2d7f
Aniket <aniketng08@gmail.com>
2023-07-07 13:10:03 +0530

    Wrote a basic UI for RPS

--- script.js
+++ script.js
@@ -1,37 +1,80 @@
 function getComputerChoice() {
   const computerChoice = Math.floor(Math.random() * 3);
-  return computerChoice;
-}
 
-function playRound(computerChoice, playerChoice) {
-  let playerChoiceStr = playerChoice.toLowerCase();
   let computerChoiceStr;
 
-  if (computerChoice == 0) computerChoiceStr = "rock";
-  else if (computerChoice == 1) computerChoiceStr = "scissor";
-  else computerChoiceStr = "paper";
+  if (computerChoice === 0) computerChoiceStr = "rock";
+  else if (computerChoice === 1) computerChoiceStr = "paper";
+  else computerChoiceStr = "scissors";
 
-  if (playerChoiceStr == computerChoiceStr) return "Draw";
-  else if (playerChoiceStr == "rock" && computerChoiceStr == "paper")
-    return "You lose! Paper beats rock.";
-  else if (playerChoiceStr == "paper" && computerChoiceStr == "scissor")
-    return "You lose! Scissor beats paper.";
-  else if (playerChoiceStr == "scissor" && computerChoiceStr == "rock")
-    return "You lose! Rock beats scissor.";
-  else if (computerChoiceStr == "rock" && playerChoiceStr == "paper")
-    return "You win! Paper beats rock.";
-  else if (computerChoiceStr == "paper" && playerChoiceStr == "scissor")
-    return "You win! Scissor beats paper.";
-  else if (computerChoiceStr == "scissor" && playerChoiceStr == "rock")
-    return "You win! Rock beats scissor.";
-
-  return "Invalid user input";
+  return computerChoiceStr;
 }
 
-function play(rounds) {
-  for (let i = 0; i < rounds; i++) {
-    let playerChoice = prompt("Rock/paper/scissor?");
-    let computerChoice = getComputerChoice();
-    alert(playRound(computerChoice, playerChoice));
+let playerScore = 0;
+let cpuScore = 0;
+let gameOver = false;
+function playRound(computerChoice, playerChoice) {
+  if (playerChoice == computerChoice) return "draw";
+  else if (playerChoice == "rock" && computerChoice == "paper") {
+    cpuScore++;
+    return "you lose! paper beats rock.";
+  } else if (playerChoice == "paper" && computerChoice == "scissors") {
+    cpuScore++;
+    return "you lose! scissors beats paper.";
+  } else if (playerChoice == "scissors" && computerChoice == "rock") {
+    cpuScore++;
+    return "you lose! rock beats scissors.";
+  } else if (computerChoice == "rock" && playerChoice == "paper") {
+    playerScore++;
+    return "you win! paper beats rock.";
+  } else if (computerChoice == "paper" && playerChoice == "scissors") {
+    playerScore++;
+    return "you win! scissors beats paper.";
+  } else if (computerChoice == "scissors" && playerChoice == "rock") {
+    playerScore++;
+    return "you win! rock beats scissors.";
   }
 }
+
+const playerBtns = document.querySelectorAll(".choice");
+const cpuSelection = document.getElementById("cpu-selection");
+
+const comments = document.getElementById("comments");
+const initCommentText = comments.textContent;
+
+const playerScoreText = document.getElementById("player-score");
+const cpuScoreText = document.getElementById("cpu-score");
+
+const resetBtn = document.getElementById('reset');
+
+playerBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (!gameOver) {
+      const playerChoice = e.target.dataset.choice;
+      const compChoice = getComputerChoice();
+      cpuSelection.textContent = compChoice;
+      comments.textContent = playRound(compChoice, playerChoice);
+      cpuScoreText.textContent = cpuScore;
+      playerScoreText.textContent = playerScore;
+
+      if (playerScore >= 5) {
+        comments.textContent = "player won!";
+        gameOver = true;
+      } else if (cpuScore >= 5) {
+        comments.textContent = "cpu won!";
+        gameOver = true;
+      }
+    }
+    else{
+      comments.textContent="reset to play again!";
+    }
+  });
+});
+resetBtn.addEventListener('click',()=>{
+  playerScore = 0;
+  cpuScore = 0;
+  playerScoreText.textContent=playerScore;
+  cpuScoreText.textContent=cpuScore;
+  comments.textContent=initCommentText;
+  gameOver=false;
+})
